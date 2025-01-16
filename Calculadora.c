8a4169ebad329928c09b50302c7461e17edaff0e
tobilou-jh <samitohb@gmail.com>
2025-01-15 22:11:45 -0500

    Calculadora modificada

--- Calculadora.c
+++ Calculadora.c
@@ -5,6 +5,7 @@ int main()
 	int i,opc;
 	float num1,num2,suma,rest,multi,divi,pot;
 	do {
+		printf("Calculadora Bridge\n");
 		printf("1.Suma\n");
 		printf("2.Resta\n");
 		printf("3.Multiplicacion\n");
@@ -30,27 +31,27 @@ int main()
 			printf("La resta entre %f y %f es %f\n",num1,num2,rest);
 			break;
 		case(3):
-			printf("Ingrese el primer numero\n");
+			printf("Ingrese el multiplicando\n");
 			scanf("%f",&num1);
-			printf("Ingrese el segundo numero\n");
+			printf("Ingrese el multiplicador\n");
 			scanf("%f",&num2);
 			multi=num1*num2;
 			printf("La multiplicacion entre %f y %f es %f\n",num1,num2,multi);
 			break;
 		case(4):
-			printf("Ingrese el primer numero\n");
+			printf("Ingrese el dividendo \n");
 			scanf("%f",&num1);
-			printf("Ingrese el segundo numero\n");
+			printf("Ingrese el divisor\n");
 			scanf("%f",&num2);
 			divi=num1/num2;
-			if(num1==0&&num2==0){
-			    printf("SYNTAX ERROR\n");
-			    
-			}else{
-			    printf("La division entre %f y %f es %f\n",num1,num2,divi);
-			    
+			if(num1==0&&num2==0) {
+				printf("SYNTAX ERROR\n");
+
+			} else {
+				printf("La division entre %f y %f es %f\n",num1,num2,divi);
+
 			}
-			
+
 			break;
 		case(5):
 			printf("Ingrese la base\n");
@@ -59,25 +60,29 @@ int main()
 			scanf("%f",&num2);
 			i=1;
 			pot=1;
-			if(num2==0){
-			    pot=1;
-			}else{
-			    do {
-				pot*=num1;
-				i++;
+			if(num1==0&&num2==0) {
+				printf("SYNTAX ERROR\n");
+			} else if(num2==0) {
+				pot=1;
+				printf("La potencia de %f y %f es %f\n",num1,num2,pot);
+			} else {
+				do {
+					pot*=num1;
+					i++;
 
-			} while(i<=num2);
+				} while(i<=num2);
+				printf("La potencia de %f y %f es %f\n",num1,num2,pot);
 			}
+
 			
-			printf("La resta de %f y %f es %f\n",num1,num2,pot);
 			break;
 		case(6):
 			break;
-		
+
 		default:
-		    printf("Opcion no valida");
+			printf("Opcion no valida");
 
 		}
-	}while(opc!=6);
+	} while(opc!=6);
 	return 0;
 }
