27b9ff244f5f6acc350217a6d1a7910189805eaf
Mathius <you@example.com>
2025-01-16 21:40:59 -0500

    Ejercicio 1 agregado

---
+++ Ejercicio_1.c
@@ -0,0 +1,6 @@
+/*
+Realizar un programa en c que cuente la cantidad de veces que se ingresan números pares y la cantidad de números impares,
+cantidad de números primos ingresados. El programa seguirá leyendo números hasta que se ingrese un número negativo o el cero. 
+Al finalizar el programa se necesita imprimir las cantidades de pares, impares y primos.
+*/
+
