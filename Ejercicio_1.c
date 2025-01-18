a4cdbb7875e51d0b1dafba1e3bc494a43810f8aa
Mathius <you@example.com>
2025-01-17 19:25:26 -0500

    Ejercicio 1 completado

--- Ejercicio_1.c
+++ Ejercicio_1.c
@@ -2,12 +2,14 @@
 Realizar un programa en c que cuente la cantidad de veces que se ingresan números pares y la cantidad de números impares,
 cantidad de números primos ingresados. El programa seguirá leyendo números hasta que se ingrese un número negativo o el cero.
 Al finalizar el programa se necesita imprimir las cantidades de pares, impares y primos.
-
 */
 
 #include <stdio.h>
 int main(){
-    int num,i_par,i_impar,primo;
+    int num,i_par,i_impar,primo,cont;
+    i_par=0;
+    i_impar=0;  
+    primo=0;   
     
     do
     {
@@ -16,22 +18,33 @@ int main(){
         printf("\n o el cero el programa terminara: ");
         scanf("%d",&num);
 
-        if(num%2==0){
-            i_par++;
-        }else{
+        if(num>=0){
+            if(num%2==0){ //El cero al momento de finalizar el programa tambien va a contarlo
+            i_par++;      // porque tambien es un numero par
+            }else{
             i_impar++;
+            }
         }
-
-        
-        
-        
+       
+        cont = 0; // Inicializar cont para cada número
+        for(int i=1; i<=num; i++){
+            if(num%i == 0){
+            cont++;
+            }
+        }
+        if(cont == 2){ // Verificar si tiene exactamente 2 divisores
+        primo++;
+        }
         
         if(num<=0){
-            printf("Saliendo del programa...");
+            printf("Saliendo del programa...");   
         }
      
     }while(num>0);
-    
-    
+
+    printf("\n La cantidad de numeros pares es: %d",i_par);
+    printf("\n La cantidad de numeros impares es: %d",i_impar);
+    printf("\n La cantidad de numeros primos es: %d",primo);
+
  return 0;
 }
